d861eac56341b784e22c7ff2f1340087241fe4b1
dmytroKarataiev <karataev.dima@gmail.com>
2016-03-24 13:40:55 -0700

    add: dependencies, lambda support

--- app/src/main/java/com/adkdevelopment/earthquakesurvival/PagerActivity.java
+++ app/src/main/java/com/adkdevelopment/earthquakesurvival/PagerActivity.java
@@ -25,17 +25,15 @@
 package com.adkdevelopment.earthquakesurvival;
 
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
 import com.adkdevelopment.earthquakesurvival.settings.SettingsActivity;
 
@@ -71,15 +69,9 @@ public class PagerActivity extends AppCompatActivity {
         mViewPager = (ViewPager) findViewById(R.id.container);
         mViewPager.setAdapter(mSectionsPagerAdapter);
 
-
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        fab.setOnClickListener(v -> Snackbar.make(v, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show());
 
     }
 
